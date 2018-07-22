d2549f287a8af755b7ee6c2c617d8da17299fde1
Yura Rizhniv <novocaine19@gmail.com>
2018-07-22 23:40:39 +0300

    emit newMessage on createMessage

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', () => {
     console.log('Connected to the server');
-
-    socket.emit('createMessage', {
-        from: 'karen',
-        text: 'hey, i am fine'
-    });
 });
 
 socket.on('disconnect', () => {
